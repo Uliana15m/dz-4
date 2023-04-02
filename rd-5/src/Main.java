4655149270e2f185c9407589dc300a4edf9f97f3
Uliana15m <4lyna1510@gmail.com>
2023-04-02 22:41:19 +0200

    domaska commit

---
+++ rd-5/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        dva vardva = new dva();
+
+        String povernene = String.valueOf(vardva.dva());
+        System.out.println("***"+povernene+"***");
+    }
+
+
+}
+
---
+++ rd-5/src/dva.java
@@ -0,0 +1,8 @@
+public class dva {
+
+    public int a = 10;
+    public int dva() {
+        System.out.println("---<"+a+">---");
+        return a;
+    }
+}
